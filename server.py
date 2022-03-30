60d6f657579598af9029592986cfe418bc5f8b24
alefiury <alefiury14@gmail.com>
2022-03-30 17:00:58 -0300

    Update

--- server.py
+++ server.py
@@ -10,8 +10,8 @@ def main() -> None:
     context = zmq.Context()
     s  = context.socket(zmq.REP)    # create reply socket
 
-    p1 = "tcp://"+ HOST +":"+ PORT1 # how and where to connect
-    p2 = "tcp://"+ HOST +":"+ PORT2 # how and where to connect
+    p1 = f"tcp://{HOST}:{PORT1}" # how and where to connect
+    p2 = f"tcp://{HOST}:{PORT2}" # how and where to connect
     s.bind(p1)                      # bind socket to address
     s.bind(p2)                      # bind socket to address
 
